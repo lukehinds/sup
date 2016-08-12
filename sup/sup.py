f8dbf5e91bf2040adaee6251998584582a6dba36
Luke Hinds <lukehinds@gmail.com>
2016-08-13 00:07:16 +0100

    Project Skeleton

--- sup/sup.py
+++ sup/sup.py
@@ -1,20 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-This is a skeleton file that can serve as a starting point for a Python
-console script. To run this script uncomment the following line in the
-entry_points section in setup.cfg:
-
-    console_scripts =
-     fibonacci = sup.skeleton:run
-
-Then run `python setup.py install` which will install the command `fibonacci`
-inside your current environment.
-Besides console scripts, the header (i.e. until _logger...) of this file can
-also be used as template for Python modules.
-
-Note: This skeleton file can be safely removed if not needed!
-"""
 from __future__ import division, print_function, absolute_import
 
 import argparse
@@ -24,7 +9,6 @@ import logging
 from sup import __version__
 
 __author__ = "Luke Hinds"
-__copyright__ = "Luke Hinds"
 __license__ = "none"
 
 _logger = logging.getLogger(__name__)
